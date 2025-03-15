f78a8ba8be8b6fa65ad139441f65e4b003fc17e4
Awaria AD <awaria@wiim.prv>
2025-03-15 12:28:02 +0100

    mkrolczyk - added e2e sample tests

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,36 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Move Slides Test', function () {
+  it('Checks if user can clik next slide and then back to previous slide', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.wait(2000);
+    cy.get('.swiper-button-prev').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+describe('Check Slides Content Test', function () {
+  it('Checks if main page slides contain correct descriptions', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.get('.card-description').should('contain', 'Rome');
+    cy.get('.card-description').should('contain', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.get('.card-description').should('contain', 'London');
+    cy.get('.card-description').should('contain', 'United Kingdom');
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.card-description').should('contain', 'Paris');
+    cy.get('.card-description').should('contain', 'France');
+  });
+});
